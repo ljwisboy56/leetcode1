546f5b8366d3c535ddae1aee357a07ce38c4f2b2
英儒 <yingru.ljw@alibaba-inc.com>
2020-02-15 13:43:32 +0800

    20200215

--- spring-boot-test/src/main/java/com/example/leetcode2/被围绕的区域.java
+++ spring-boot-test/src/main/java/com/example/leetcode2/被围绕的区域.java
@@ -1,5 +1,7 @@
 package com.example.leetcode2;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -81,19 +83,79 @@ public class 被围绕的区域 {
 
         Stack<Pos> stack = new Stack<>();
 
+        board[i][j] = '#';
         stack.push(new Pos(i,j));
 
         while (!stack.isEmpty()){
 
+            Pos current = stack.pop();
+            if (current.i - 1 >= 0
+                    && board[current.i - 1][current.j] == 'O') {
+                stack.push(new Pos(current.i - 1, current.j));
+                board[current.i - 1][current.j] = '#';
+            }
 
-
-
-
+            // 下
+            if (current.i + 1 <= board.length - 1
+                    && board[current.i + 1][current.j] == 'O') {
+                stack.push(new Pos(current.i + 1, current.j));
+                board[current.i + 1][current.j] = '#';
+            }
+            // 左
+            if (current.j - 1 >= 0
+                    && board[current.i][current.j - 1] == 'O') {
+                stack.push(new Pos(current.i, current.j - 1));
+                board[current.i][current.j - 1] = '#';
+            }
+            // 右
+            if (current.j + 1 <= board[0].length - 1
+                    && board[current.i][current.j + 1] == 'O') {
+                stack.push(new Pos(current.i, current.j + 1));
+                board[current.i][current.j + 1] = '#';
+            }
         }
-
-
-
     }
+
+    /**
+     * 图的bfs用队列做广度优先遍历
+     * @param board
+     * @param i
+     * @param j
+     */
+    public void bfs(char[][] board, int i, int j) {
+        Queue<Pos> queue = new LinkedList<>();
+        queue.add(new Pos(i, j));
+        board[i][j] = '#';
+        while (!queue.isEmpty()) {
+            Pos current = queue.poll();
+            // 上
+            if (current.i - 1 >= 0
+                    && board[current.i - 1][current.j] == 'O') {
+                queue.add(new Pos(current.i - 1, current.j));
+                board[current.i - 1][current.j] = '#';
+                // 没有continue.
+            }
+            // 下
+            if (current.i + 1 <= board.length - 1
+                    && board[current.i + 1][current.j] == 'O') {
+                queue.add(new Pos(current.i + 1, current.j));
+                board[current.i + 1][current.j] = '#';
+            }
+            // 左
+            if (current.j - 1 >= 0
+                    && board[current.i][current.j - 1] == 'O') {
+                queue.add(new Pos(current.i, current.j - 1));
+                board[current.i][current.j - 1] = '#';
+            }
+            // 右
+            if (current.j + 1 <= board[0].length - 1
+                    && board[current.i][current.j + 1] == 'O') {
+                queue.add(new Pos(current.i, current.j + 1));
+                board[current.i][current.j + 1] = '#';
+            }
+        }
+    }
+
 
     static public class Pos{
         int i;
